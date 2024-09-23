919fb983a009a833da8f1d430675645b47d602ff
Alexandre Henriet <alexandre.henriet@infrabel.be>
2024-09-23 15:27:23 +0200

    test gh-pages deployment 2

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -19,7 +19,7 @@ const config: Config = {
   projectName: 'debian', // Usually your repo name.
   deploymentBranch: 'master',
   trailingSlash: false,
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internationalization, you can use this field to set
